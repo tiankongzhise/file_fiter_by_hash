0d03cf92112320cda2c13f22aca0030b6aea4f3d
hbc_thinkbook <419337244@qq.com>
2026-01-08 22:39:02 +0800

    feat 重构了分类的配置文件，完成了成型运营主要逻辑规划

--- src/file_fiter_by_hash/config/classify_config.py
+++ src/file_fiter_by_hash/config/classify_config.py
@@ -6,24 +6,11 @@ class ClassifyConfig:
     # 记录需要处理的源文件夹或者源文件
     sources_list:list[str] = [r'']
     
-    # 经过检测，无特殊情况，没有备份过的项目,需要被压缩的，被转移到此文件夹
-    normal_folder: str = r'D:\待压缩后备份'
-    
-    # 没有备份过，被压缩过后，生成的文件在此文件夹
-    upload_folder: str = r'D:\待上传'
+    # 压缩后的结果保存到此文件夹
+    zipped_folder: str = r''
 
-    # 文件数量超过阈值的文件夹被转移到此文件夹
-    overcount_folder: str = r'D:\数量超过阈值'
-    
-    # 文件或文件夹大小超过阈值的被转移到此文件夹
-    oversize_folder: str = r'D:\大小超过阈值'
-    
-    # 已经被备份过的文件,等待被删除
-    duplicate_folder: str = r'D:\与备份文件重复'
-    
-    # 本次处理过程中发现的重复文件,移动到此文件夹,等待处理
-    local_duplicate_folder: str = r'D:\本地重复'
-    
+    # 将压缩后的文件解压到此文件夹与源文件进行比较
+    rezip_folder: str = r''
     
     # 压缩时每次压缩的文件大小阈值，单位字节
     max_processing_file_size: int = 1024 * 1024 * 500 # 500M
--- src/file_fiter_by_hash/control/filter_process.py
+++ src/file_fiter_by_hash/control/filter_process.py
@@ -1,1 +1,53 @@
-from ..config.file_config import FileConfig
+from tkinter import Pack
+from ..config.classify_config import ClassifyConfig
+from ..service.classfiy_service import classify_folder,classify_item
+import pathlib
+def pre_classify(config: ClassifyConfig):
+    result = {}
+    for item in config.sources_list:
+        temp = pathlib.Path(item)
+        if temp in result:
+            continue
+        if temp.is_dir():
+            result[temp] = classify_folder(temp)
+        elif temp.is_file():
+            result[temp] = classify_item(temp)
+    return result
+
+def save_pre_classify_result(pre_classify_result: dict):
+    pass
+
+def calculate_hash_result(save_pre_classify_result: dict):
+    pass
+
+def save_hash_result(hash_result: dict):
+    pass
+
+def zip_item(item_info):
+    pass
+def save_zip_result(zip_result: dict):
+    pass
+def calculate_ziped_item_hash(zip_item):
+    pass
+def save_ziped_item_hash(ziped_item_hash: dict):
+    pass
+def rezip_item(zip_item):
+    pass
+def save_rezip_result(rezip_result: dict):
+    pass
+def compare_rezip_source():
+    pass
+def save_compare_result(compare_result: dict):
+    pass
+def upload_ziped_file(zip_file):
+    pass
+def save_upload_result(upload_result: dict):
+    pass
+def del_zipped_file(zip_file):
+    pass
+def save_del_result(del_result: dict):
+    pass
+def del_source_file():
+    pass
+def save_del_source_result(del_source_result: dict):
+    pass
